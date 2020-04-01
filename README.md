647226caa1c89f7d5ff5bd64b40d053810d4f667
Bryan <bryan.fennell@hotmail.com>
2020-04-01 17:43:16 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gw-homework03
+
+## Synopsis
+
+This project is the deliverable for the **JavaScript** lesson's homework.
+
+It presents a simple web form from which users can generate passwords composed of random numbers.
+
+The criteria for these passwords are as follows:
+- The password length must be between 8 and 128 characters, inclusive.
+- The user must choose to include at least one of the following categories of characters:
+  - Uppercase letters
+  - Lowercase letters
+  - Numbers
+  - Special characters (as specified in [OWASP's list](https://www.owasp.org/index.php/Password_special_characters))
+
+To use use this program, go to [the site](https://bryan3023.github.io/gw-homework03/) and click the *Generate Password* button. Then answer the sequence of prompts until a password is generated for you.
+
+You can also watch the Developer Tools Console to confirm you input is processed correctly.
---
+++ assets/script.js
@@ -0,0 +1,90 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+function promptPasswordLength() {
+  let
+    min = 8,
+    max = 128,
+    length;
+
+  do {
+    length = prompt(
+      "How long do you want the password to be? Choose between " +
+      min + " and " + max + " characters."
+    );
+    length = parseInt(length);
+  } while (isNaN(length) || length < min || length > max);
+
+  console.log(" - Password length will be %i", length);
+
+  return length;
+}
+
+function confirmIncludeCharacters(characterName, characters) {
+  
+  shouldInclude = confirm(
+    "Would you like to include " +
+    characterName.toLowerCase() + "?"
+  );
+
+  if (shouldInclude) {
+    console.log(" - %s will be included.", characterName);
+    return characters;
+  } else {
+    console.log(" - %s will not be included.", characterName);
+    return "";
+  }
+}
+
+function generatePassword() {
+  const
+    letters = "abcdefghijklmnopqrstuvwxyz",
+    numbers = "0123456789",
+    special = " `~!@#$%^&*()_+-=[]\\{}|;':\",./<>?";
+
+  let
+    length,
+    includedCharacters,
+    password = "";
+
+  console.log("Begin generating password:");
+
+  length = promptPasswordLength();
+ 
+  includedCharacters = confirmIncludeCharacters("Lowercase letters", letters);
+  includedCharacters += confirmIncludeCharacters("Uppercase letters", letters.toUpperCase());
+  includedCharacters += confirmIncludeCharacters("Numbers", numbers);
+  includedCharacters += confirmIncludeCharacters("Special characters", special);
+
+  if (0 === includedCharacters.length) {
+    alert("You must include at least one type of character to generate a password!");
+    return "";
+  }
+
+  for (let i = 0; i < length; i++) {
+    password += includedCharacters[getRandomInt(0, includedCharacters.length - 1)];
+  }
+
+  console.log("Done!");
+
+  return password;
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
